b154ca12e58a02f14c045ac48618fcbbcd2e5432
Mohannad Adnan Alassar <mohannad.alassar@hotmail.com>
2024-11-17 17:52:51 +0300

    add padding

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
   }, [lastScrollTop]);
 
   return (
-    <main>
+    <main className=" py-20">
       <div className="w-full flex flex-row justify-center items-center relative z-30 pb-[5rem] pt-[5rem] lg:pt-[10rem] lg:pb-[10rem] overflow-clip">
         <div className="relative rotate-[-7.1deg] flex-shrink-0 origin-center flex flex-col items-start overflow-clip justify-start gap-[0.5rem] lg:gap-[0.8rem] w-[130vw] max-w-[unset]">
           <div className="absolute top-[-3rem] start-0 w-full h-[10rem] lg:h-[24rem] from-white to-transparent bg-gradient-to-b z-[10]"></div>
